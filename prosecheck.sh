17b2b7950cd8896792421e51f1fdca9e731205f5
denten <denten@gmail.com>
2017-03-23 15:06:07 -0400

    add safety flags

--- prosecheck.sh
+++ prosecheck.sh
@@ -1,12 +1,10 @@
-#!/bin/sh
+#!/bin/sh -eu
 
 # "set -e" is short for "set -o errexit", that is, abort the script if a
 # command returns with a non-zero exit code.
 
 # "set -u" is short for "set -o nounset", that is, abort the script if a
 # variable name is dereferenced when the variable hasn't been set.ty
-
-set -eu
 
 # Usage:
 #           sh prosereport.sh [-s|-l] filename.md
